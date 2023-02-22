bc46e677d6ef9f9fd86dd27f2f89382038929bd1
mmolins <mmolins@minsait.com>
2023-02-22 15:36:33 +0100

    fix

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -3,8 +3,7 @@ import '../css/componentes.css';
 
 
 export const saludar = ( nombre = 'sin nombre' ) => {
-    console.log('Creando etiqueta h1');
-
+    
     const h1 = document.createElement('h1');
     h1.innerText = `Hola ${ nombre }`;
 
